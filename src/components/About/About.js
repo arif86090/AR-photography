e531a75da969295a2aaa3b33f83ea22f239abe43
Arif <aharif281@gmail.com>
2022-04-17 21:27:21 +0600

    about edit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                 <div className='about-me'>
                     <h2>HELLO, MY NAME IS <span>MD ARIF HOSSAIN</span></h2>
                     <p>I Have Been Designing And Developing Website For 4 months.i  try my best  to learn this language. daily i am working 8 hours for learning . if i work hard one day i will success.so i can't waste my time any where withuot coading. i have learned html,css,Bootstrap,tailwind css, simple javascript and now react js. It Will Be Responsive, Mobile-Friendly.If You Need To Build A Full Complete Website With More Functions And Unque Look For Your Service/Business/Protfolio, Please Contact Me</p>
-                    <button>Learn more</button>
+                    <button>Learn More</button>
                 </div>
             </div>
             <div className='col-md-6 mt-5 align-self-center text-center'>
